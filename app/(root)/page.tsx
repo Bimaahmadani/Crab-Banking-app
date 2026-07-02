b2e7c35a50be712dadb17113ef4e6c7c5dc5b889
Bima Ahmadani <bimaahmadani@gmail.com>
2026-07-02 15:27:41 +0700

    git commit --amendadd HeaderBox component, update root/page.sx add HeaderBox component, update root/page.tsx

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,22 @@
+import HeaderBox from "@/components/HeaderBox";
 import React from "react";
 
 const Home = () => {
+    const loggedIn = {firstName: "John" };
+
     return (
-        <div>Home</div>
+        <section className="home">
+            <div className="home-content">
+                <header>
+                    <HeaderBox 
+                        type="greeting"
+                        title="Welcome"
+                        user= {loggedIn?.firstName || "Guest"}
+                        subtext="Your trusted financial partner. Explore our services and manage your finances with ease."
+                    />
+                </header>
+            </div>
+        </section>
     )
 }
 
