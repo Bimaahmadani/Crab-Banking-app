e3318e0809eebf8991ef70b11dea827f91e34bf4
Bima Ahmadani <bimaahmadani@gmail.com>
2026-07-11 13:42:58 +0700

    update Sidebar

--- components/ui/Sidebar.tsx
+++ components/ui/Sidebar.tsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
 
-const Sidebar = () => {
+const Sidebar = ({user}:SiderbarProps) => {
   return (
-    <div>Sidebar</div>
+    <section className="sidebar">
+        <nav className="flex flex-col gap-4">
+            <Link href="/"
+            className='mb-12
+            cursor-pointer
+            items-center gap-2
+            '
+            >
+                <Image
+                src="/images/logo.png"
+                width={34}
+                height={34}
+                alt="CrabBank Logo"
+                className='size-[24px]
+                max-xl:size-14'
+                >
+                    <h1 className='sidebar-logo'>CrabBank</h1>
+                </Image>
+            </Link>
+        </nav>
+    </section>
   )
 }
 
